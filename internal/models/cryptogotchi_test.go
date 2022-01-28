a25e9046785587e943eccf6ac50c46ca7b3b6a00
Tim-Lukas Bastin <tim-lukas.bastin@wetteronline.de>
2022-01-28 08:23:27 +0100

    Fixed tests

--- internal/models/cryptogotchi_test.go
+++ internal/models/cryptogotchi_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"gitlab.com/l3montree/cryptogotchi/clodhopper/internal/config"
 	"gitlab.com/l3montree/cryptogotchi/clodhopper/internal/models"
 	"gitlab.com/l3montree/cryptogotchi/clodhopper/internal/util"
 )
@@ -16,9 +17,12 @@ func TestCryptogotchiProgressUntil(t *testing.T) {
 		FoodDrain: 1,
 		Events:    []models.Event{},
 		GameStats: []models.GameStat{},
+		// set the snapshot validity to now - 10 minutes
+		SnapshotValid: time.Now().Add(time.Minute * -10),
 		Base: models.Base{
 			CreatedAt: time.Now().Add(time.Minute * -10),
 		},
+		PredictedDeathDate: time.Now().Add(100 * time.Minute),
 	}
 
 	cryptogotchi.ProgressUntil(time.Now())
@@ -37,29 +41,25 @@ func TestDeathDate(t *testing.T) {
 		Base: models.Base{
 			CreatedAt: time.Now().Add(time.Minute * -100),
 		},
+		// died 10 minutes ago
+		PredictedDeathDate: time.Now().Add(-10 * time.Minute),
 	}
 
 	isAlive, deathDate := cryptogotchi.ProgressUntil(time.Now())
+
 	assert.False(t, isAlive)
-	assert.Equal(t, time.Now().Add(time.Minute*-90).Unix(), deathDate.Unix())
+	assert.Equal(t, time.Now().Add(time.Minute*-10).Unix(), deathDate.Unix())
 }
 
-func generateFeedEvent(createdAt time.Time) models.Event {
-	return models.Event{
-		Type: models.FeedEventType,
-		Base: models.Base{
-			CreatedAt: createdAt,
-		},
-	}
-}
 func TestNextFeedingTime(t *testing.T) {
 	cryptogotchi := models.Cryptogotchi{
 		Name:      util.Str("Tabito"),
 		Food:      10,
 		FoodDrain: 1,
 
-		Events:    []models.Event{},
-		GameStats: []models.GameStat{},
+		Events:             []models.Event{},
+		GameStats:          []models.GameStat{},
+		PredictedDeathDate: time.Now().Add(100 * config.TIME_BETWEEN_FEEDINGS),
 		Base: models.Base{
 			CreatedAt: time.Now().Add(time.Minute * -10),
 		},
@@ -71,12 +71,12 @@ func TestNextFeedingTime(t *testing.T) {
 
 	// the cryptogotchi was feed 5 minutes ago
 	// the next feeding time would be - in 5 minutes
-	cryptogotchi.Events = append(cryptogotchi.Events, generateFeedEvent(time.Now().Add(time.Minute*-5)))
-	nextFeeding = cryptogotchi.GetNextFeedingTime()
-	assert.Equal(t, time.Now().Add(time.Minute*5).Unix(), nextFeeding.Unix())
+	event := models.Event{
+		Type: models.FeedEventType,
+	}
+	event.Apply(&cryptogotchi)
 
-	// the cryptogotchi was feed 20 minutes ago and 5 minutes ago - the next feeding time would still be in 5 minutes
-	cryptogotchi.Events = append(cryptogotchi.Events, generateFeedEvent(time.Now().Add(time.Minute*-20)))
 	nextFeeding = cryptogotchi.GetNextFeedingTime()
-	assert.Equal(t, time.Now().Add(time.Minute*5).Unix(), nextFeeding.Unix())
+	// feeding time should be now + 10 minutes (or whatever the config value is set to)
+	assert.Equal(t, time.Now().Add(config.TIME_BETWEEN_FEEDINGS).Unix(), nextFeeding.Unix())
 }
