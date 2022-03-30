28250b05b0bd57a306a8e5fb09844995426463f1
timbastin <bastin.tim@gmail.com>
2022-03-30 09:11:10 +0200

    Removed TestGeneration

--- internal/generator/generator_test.go
+++ internal/generator/generator_test.go
@@ -7,23 +7,6 @@ import (
 	"testing"
 )
 
-/*func TestGeneration(t *testing.T) {
-	path, err := filepath.Abs(filepath.Join("..", "..", "images", "raw"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rand.Seed(42)
-	preloader := NewMemoryPreloader(path)
-	generator := NewGenerator(preloader)
-
-	for i := 0; i < 10; i++ {
-		f, _ := os.Create(fmt.Sprintf("%d.png", i))
-		img := generator.TokenId2Image(fmt.Sprintf("%d", rand.Int()))
-		png.Encode(f, img)
-	}
-}*/
-
 // BenchmarkGeneration-8   	       2	 609847462 ns/op	219332280 B/op	19943308 allocs/op
 // BenchmarkGeneration-8   	       2	 640104734 ns/op	219332176 B/op	19943308 allocs/op
 // BenchmarkGeneration-8   	       3	 460615302 ns/op	200263784 B/op	19942925 allocs/op
