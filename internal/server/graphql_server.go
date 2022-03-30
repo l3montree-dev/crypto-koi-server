6abb312974a0c4c81f817e5e6458867c2a093912
timbastin <bastin.tim@gmail.com>
2022-03-30 10:39:46 +0200

    Non blocking image cache generation

--- internal/server/graphql_server.go
+++ internal/server/graphql_server.go
@@ -151,8 +151,12 @@ func graphqlTimeout(timeout time.Duration) func(next http.Handler) http.Handler 
 	}
 }
 func NewGraphqlServer(db *gorm.DB, imagesBasePath string) Server {
-	koiPreloader := generator.NewMemoryPreloader(imagesBasePath+"/koi").BuildCachesForSizes(1024, 200)
-	dragonPreloader := generator.NewMemoryPreloader(imagesBasePath+"/dragon").BuildCachesForSizes(1024, 200)
+	koiPreloader := generator.NewMemoryPreloader(imagesBasePath + "/koi")
+	dragonPreloader := generator.NewMemoryPreloader(imagesBasePath + "/dragon")
+
+	go koiPreloader.BuildCachesForSizes(1024, 200)
+	go dragonPreloader.BuildCachesForSizes(1024, 200)
+
 	return &GraphqlServer{
 		db:              db,
 		koiGenerator:    generator.NewGenerator(koiPreloader),
